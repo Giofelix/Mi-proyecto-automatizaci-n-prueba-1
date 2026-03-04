d89b00d1c9b78b1c3c8857a6b9cf0f4da247d489
Giovani <giofelix_21@hotmail.com>
2026-03-04 12:44:48 -0500

    Prueba

--- playwright.config.js
+++ playwright.config.js
@@ -14,7 +14,6 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests/iu',
-  testMatch: '**/iu/**/*.spec.js',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
